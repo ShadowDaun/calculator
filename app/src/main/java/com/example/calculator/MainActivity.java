991cbade903606c4823f481d3cf02b13283c9b43
nekabr <q123dbb-r@ya.ru>
2022-12-19 16:36:57 +0300

    ez

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1, b2, b3, plus, result;
+    Button b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, plus, minus, result;
     TextView tv1;
     int ch1;
     String operator;
@@ -20,13 +20,75 @@ public class MainActivity extends AppCompatActivity {
         b1 = (Button) findViewById(R.id.b1);
         b2 = (Button) findViewById(R.id.b2);
         b3 = (Button) findViewById(R.id.b3);
+        b4 = (Button) findViewById(R.id.b4);
+        b5 = (Button) findViewById(R.id.b5);
+        b6 = (Button) findViewById(R.id.b6);
+        b7 = (Button) findViewById(R.id.b7);
+        b8 = (Button) findViewById(R.id.b8);
+        b9 = (Button) findViewById(R.id.b9);
+        b0 = (Button) findViewById(R.id.b0);
         plus = (Button) findViewById(R.id.plus);
+        minus = (Button) findViewById(R.id.minus);
         result = (Button) findViewById(R.id.res);
         tv1 = (TextView) findViewById(R.id.textView);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 tv1.setText(tv1.getText() + "1");
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv1.setText(tv1.getText() + "2");
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv1.setText(tv1.getText() + "3");
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv1.setText(tv1.getText() + "4");
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv1.setText(tv1.getText() + "5");
+            }
+        });
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv1.setText(tv1.getText() + "6");
+            }
+        });
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv1.setText(tv1.getText() + "7");
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv1.setText(tv1.getText() + "8");
+            }
+        });
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv1.setText(tv1.getText() + "9");
+            }
+        });
+        b0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv1.setText(tv1.getText() + "0");
             }
         });
         plus.setOnClickListener(new View.OnClickListener() {
@@ -38,11 +100,23 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operator = "-";
+                ch1 = Integer.parseInt((String) tv1.getText());
+                tv1.setText("");
+            }
+
+        });
         result.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (operator.equals("+")) {
                     tv1.setText(String.valueOf(ch1 + Integer.parseInt((String) tv1.getText())));
+                }
+                if (operator.equals("-")) {
+                    tv1.setText(String.valueOf(ch1 - Integer.parseInt((String) tv1.getText())));
                 }
             }
 

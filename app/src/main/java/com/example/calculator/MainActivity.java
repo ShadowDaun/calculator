9e0c02a5a668a0a76f766961238e6ef6082f3c8b
nekabr <q123dbb-r@ya.ru>
2022-12-22 16:37:24 +0300

    ez

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,10 +8,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, plus, minus, result;
+    Button b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, plus, minus, ymn, result, del,
+            ster, zap, pm, sk;
     TextView tv1;
-    int ch1;
+    Double ch1;
     String operator;
+    int m = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,13 @@ public class MainActivity extends AppCompatActivity {
         b9 = (Button) findViewById(R.id.b9);
         b0 = (Button) findViewById(R.id.b0);
         plus = (Button) findViewById(R.id.plus);
+        ymn = (Button) findViewById(R.id.ymn);
         minus = (Button) findViewById(R.id.minus);
+        del = (Button) findViewById(R.id.del);
+        ster = (Button) findViewById(R.id.ster);
+        zap = (Button) findViewById(R.id.zap);
+        pm = (Button) findViewById(R.id.pm);
+        sk = (Button) findViewById(R.id.sk);
         result = (Button) findViewById(R.id.res);
         tv1 = (TextView) findViewById(R.id.textView);
         b1.setOnClickListener(new View.OnClickListener() {
@@ -91,33 +99,92 @@ public class MainActivity extends AppCompatActivity {
                 tv1.setText(tv1.getText() + "0");
             }
         });
+        zap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {tv1.setText(tv1.getText() + ".");}
+        });
+        sk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {tv1.setText(tv1.getText() + "(");}
+
+        });
+        sk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {tv1.setText(tv1.getText() + ")");}
+
+        });
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 operator = "+";
-                ch1 = Integer.parseInt((String) tv1.getText());
+                ch1 = Double.parseDouble((String) tv1.getText());
                 tv1.setText("");
             }
-
         });
         minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 operator = "-";
-                ch1 = Integer.parseInt((String) tv1.getText());
+                ch1 = Double.parseDouble((String) tv1.getText());
                 tv1.setText("");
             }
 
+        });
+        ymn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operator = "*";
+                ch1 = Double.parseDouble((String) tv1.getText());
+                tv1.setText("");
+            }
+
+        });
+        del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operator = "/";
+                ch1 = Double.parseDouble((String) tv1.getText());
+                tv1.setText("");
+            }
+        });
+        ster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operator = "C";
+                ch1 = Double.parseDouble((String) tv1.getText());
+                tv1.setText("");
+            }
+        });
+        pm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operator = "+-";
+                ch1 = Double.parseDouble((String) tv1.getText());
+                tv1.setText(String.valueOf(ch1 * -1 ));
+            }
         });
         result.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (operator.equals("+")) {
-                    tv1.setText(String.valueOf(ch1 + Integer.parseInt((String) tv1.getText())));
+                    tv1.setText(String.valueOf(ch1 + Double.parseDouble((String) tv1.getText())));
                 }
                 if (operator.equals("-")) {
-                    tv1.setText(String.valueOf(ch1 - Integer.parseInt((String) tv1.getText())));
+                    tv1.setText(String.valueOf(ch1 - Double.parseDouble((String) tv1.getText())));
                 }
+                if (operator.equals("*")) {
+                    tv1.setText(String.valueOf(ch1 * Double.parseDouble((String) tv1.getText())));
+                }
+                if (operator.equals("/")) {
+                    tv1.setText(String.valueOf(ch1 / Double.parseDouble((String) tv1.getText())));
+                }
+                if (operator.equals("C")) {
+                    tv1.setText("");
+                }
+                if (operator.equals("+-")) {
+                    tv1.setText(String.valueOf(ch1 * -1 ));
+                    }
+
             }
 
         });
